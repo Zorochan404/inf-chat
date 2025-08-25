6bd78124f7e10dcf408e4e4295ebcf196eaa4f93
Zorochan404 <dram8898@gmail.com>
2025-08-26 02:05:05 +0530

    added more routes to groups

--- src/studyGroup/studyGroupController.ts
+++ src/studyGroup/studyGroupController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { studyGroup } from "./studyGroupModel.js";
+import { User } from "../auth/userModel.js";
 
 export const addGroup = async (req: Request, res: Response) => {
   try {
@@ -24,6 +25,101 @@ export const addGroup = async (req: Request, res: Response) => {
         message: "Unauthorized",
       });
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+    console.log(error);
+  }
+};
+
+export const editGroup = async (req: Request, res: Response) => {
+  try {
+    const { groupId } = req.query;
+    const group = await studyGroup.findOneAndUpdate(
+      { _id: groupId, created_by: (req as any).user.userId },
+      req.body,
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Group updated successfully",
+      data: group,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+    console.log(error);
+  }
+};
+
+export const getGroupById = async (req: Request, res: Response) => {
+  try {
+    const group = await studyGroup.findOne({
+      _id: req.query.id,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Group found successfully",
+      data: group,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+    console.log(error);
+  }
+};
+
+export const deleteGroup = async (req: Request, res: Response) => {
+  try {
+    if (
+      (req as any).user.role !== "admin" &&
+      (req as any).user.role !== "teacher"
+    ) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const group = await studyGroup.findByIdAndDelete({
+      _id: (req as any).query.id,
+      created_by: (req as any).user.userId,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Group deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+    console.log(error);
+  }
+};
+
+export const myGroups = async (req: Request, res: Response) => {
+  try {
+    const groups = await studyGroup.find({
+      created_by: (req as any).user.userId,
+    });
+
+    if (!groups) {
+      return res.status(404).json({
+        success: false,
+        message: "No groups found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Groups fetched successfully",
+      data: groups,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
--- src/studyGroup/studyGroupRoutes.ts
+++ src/studyGroup/studyGroupRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/auth.js";
-import { addGroup } from "./studyGroupController.js";
+import { addGroup, deleteGroup, editGroup, getGroupById, myGroups } from "./studyGroupController.js";
 
 const router = Router();
 
 router.post("/addgroup", authenticateToken, addGroup);
+router.put("/updatedgroup", authenticateToken, editGroup);
+router.get("/getgroup", authenticateToken, getGroupById);
+router.delete("/deletegroup", authenticateToken, deleteGroup);
+router.get("/mygroups", authenticateToken, myGroups)
 
-
-export default router;
+export default router;  
